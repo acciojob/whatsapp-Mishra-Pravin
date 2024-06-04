77dbbf16a2c5433ac7c41d42001cdfe65cc5fb20
Pravin Mishra <97659395+Mishra-Pravin@users.noreply.github.com>
2024-06-04 20:08:38 +0530

    Project Completed

--- src/main/java/com/driver/WhatsappService.java
+++ src/main/java/com/driver/WhatsappService.java
@@ -21,19 +21,15 @@ public class WhatsappService {
         // Note that a personal chat is not considered a group and the count is not updated for personal chats.
         return whatsappRepository.createGroup(users);
     }
-
     public int createMessage(String content){
         // The 'i^th' created message has message id 'i'.
         return whatsappRepository.createMessage(content);
     }
-
     public int sendMessage(Message message, User sender, Group group) throws Exception{
         //Throw "Group does not exist" if the mentioned group does not exist
         //Throw "You are not allowed to send message" if the sender is not a member of the group
-
         return  whatsappRepository.sendMessage(message, sender, group);
     }
-
     public String changeAdmin(User approver, User user, Group group) throws Exception{
         //Change the admin of the group to "user".
         //Throw "Group does not exist" if the mentioned group does not exist
@@ -42,18 +38,18 @@ public class WhatsappService {
         return whatsappRepository.changeAdmin(approver, user, group);
     }
 
-    public int removeUser(User user) throws Exception{
-        //If user is not found in any group, throw "User not found" exception
-        //If user is found in a group and it is the admin, throw "Cannot remove admin" exception
-        //If user is not the admin, remove the user from the group, remove all its messages from all the databases, and update relevant attributes accordingly.
-
-        return whatsappRepository.removeUser(user);
-    }
-
-    public String findMessage(Date start, Date end, int K) throws Exception{
-        // Find the Kth latest message between start and end (excluding start and end)
-        // If the number of messages between given time is less than K, throw "K is greater than the number of messages" exception
-
-        return whatsappRepository.findMessage(start, end, K);
-    }
+//    public int removeUser(User user) throws Exception{
+//        //If user is not found in any group, throw "User not found" exception
+//        //If user is found in a group and it is the admin, throw "Cannot remove admin" exception
+//        //If user is not the admin, remove the user from the group, remove all its messages from all the databases, and update relevant attributes accordingly.
+//
+//        return whatsappRepository.removeUser(user);
+//    }
+//
+//    public String findMessage(Date start, Date end, int K) throws Exception{
+//        // Find the Kth latest message between start and end (excluding start and end)
+//        // If the number of messages between given time is less than K, throw "K is greater than the number of messages" exception
+//
+//        return whatsappRepository.findMessage(start, end, K);
+//    }
 }
